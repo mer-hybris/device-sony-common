0ff941b72ceb31ec8683348440c5f5d4a338608a
Simonas Leleiva <simonas.leleiva@meramo.co.uk>
2017-02-14 10:31:53 +0000

    (hybris) Disable init_sony
    Signed-off-by: Simonas Leleiva <simonas.leleiva@jollamobile.com>

--- init/Android.mk
+++ init/Android.mk
@@ -22,21 +22,21 @@ $(info device-sony-common-init: init_sony for "$(TARGET_DEVICE)", platform "$(PR
 
 # Init overrides for init_sony
 root_init      := $(TARGET_ROOT_OUT)/init
-root_init_real := $(TARGET_ROOT_OUT)/init.real
+root_init_sony := $(TARGET_ROOT_OUT)/sbin/droid-hal-init_sony
 
-# If /init is a file and not a symlink then rename it to /init.real
-# and make /init be a symlink to /sbin/init_sony (which will execute
-# /init.real, if appropriate.
-$(root_init_real): $(root_init) $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/utilities/init_sony
+# If /sbin/droid-hal-init_sony is not a symlink then
+# make /sbin/init_sony be a symlink to it. Currently it is not
+# executed by anything, as it's in early stages to be integrated
+# with Sailfish OS
+$(root_init_sony): $(root_init) $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/utilities/init_sony
 	cp $(PRODUCT_OUT)/utilities/toybox $(TARGET_ROOT_OUT)/sbin/toybox_init
 	cp $(PRODUCT_OUT)/utilities/keycheck $(TARGET_ROOT_OUT)/sbin/keycheck
 	cp $(PRODUCT_OUT)/utilities/init_sony $(TARGET_ROOT_OUT)/sbin/init_sony
-	$(hide) if [ ! -L $(root_init) ]; then \
-	  echo "/init $(root_init) isn't a symlink"; \
-	  mv $(root_init) $(root_init_real); \
-	  ln -s sbin/init_sony $(root_init); \
+	$(hide) if [ ! -L $(root_init_sony) ]; then \
+	  echo "init_sony $(root_init_sony) isn't a symlink"; \
+	  ln -s init_sony $(root_init_sony); \
 	else \
-	  echo "/init $(root_init) is already a symlink"; \
+	  echo "init_sony $(root_init_sony) is already a symlink"; \
 	fi
 
-ALL_DEFAULT_INSTALLED_MODULES += $(root_init_real)
+ALL_DEFAULT_INSTALLED_MODULES += $(root_init_sony)
